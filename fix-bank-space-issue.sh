c5ffe34c46b6a3bc190980e5613c443b68f27da2
Ansible automation codes <144895375+ansible-codes@users.noreply.github.com>
2023-11-08 12:40:09 +0000

    Update fix-bank-space-issue.sh

--- fix-bank-space-issue.sh
+++ fix-bank-space-issue.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Function to fix file names
+# Function to fix file names by replacing spaces with underscores
 fix_file_name() {
     local file="$1"
-    local new_name="$(echo $file | tr ' ' '_')" # Replace spaces with underscores
+    local new_name="${file// /_}" # Replace spaces with underscores
     if [[ "$file" != "$new_name" ]]; then
         mv "$file" "$new_name"
-        echo "Renamed file: $file -> $new_name"
+        echo "Renamed file: '$file' -> '$new_name'"
     fi
 }
 
@@ -18,15 +18,12 @@ run_sqlplus() {
     sqlplus username/password@db @"$file"
 }
 
-export -f fix_file_name
-export -f run_sqlplus
-
-# Find all .sql files with spaces in their names
-find . -name "*.sql" -type f | grep ' ' | while read -r file; do
+# Loop through and fix file names with spaces
+find . -name "*.sql" -type f | grep ' ' | while IFS= read -r file; do
     fix_file_name "$file"
 done
 
 # Now, run sqlplus with all fixed .sql files
-find . -name "*.sql" -type f | while read -r file; do
+find . -name "*.sql" -type f -print0 | while IFS= read -r -d $'\0' file; do
     run_sqlplus "$file"
 done
